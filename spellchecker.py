e47cc4e4c5b149c8d8311ae3c451a219a02bfd87
Evan Gubarev <evan@evangubarev.com>
2020-11-12 22:50:01 -0800

    Update spellchecker.py

--- spellchecker.py
+++ spellchecker.py
@@ -4,16 +4,19 @@ Created 15-Mar-20
 """
 try:
     from similar import findsimilar
+
     print()
     print("Welcome to the KD7T Spellcheker")
     print()
     import json
+
     top = 0
     bot = 0
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    
-    def convert(lst):                     #just stuff cause json sucks
-        return (lst[0].split())
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    def convert(lst):  # just stuff cause json sucks
+        return lst[0].split()
+
     with open("dict.json", "r") as f:
         jsontext = json.load(f)
     words = jsontext
@@ -22,8 +25,9 @@ try:
     textstring = input(">> ")
     print()
     print()
-    textarray =  eval('["' + textstring + '"]')
+    textarray = eval('["' + textstring + '"]')
     textwords = convert(textarray)
+
     def capital(string):
         capitalarray = []
         capitalarray.append(string)
@@ -32,23 +36,29 @@ try:
             return True
         else:
             return False
+
     def ends(string):
         endsarray = []
         endsarray.append(string)
         lastletter = endsarray[0][-1]
-        if lastletter == "." or lastletter == "," or lastletter == "?" or lastletter == "!":
+        if (
+            lastletter == "."
+            or lastletter == ","
+            or lastletter == "?"
+            or lastletter == "!"
+        ):
             return True
         else:
-            return False    
-        
+            return False
+
     for word in textwords:
-        if (word in words):
-            #Spelled correctly
+        if word in words:
+            # Spelled correctly
             print("Good Word    --------> " + word)
             top += 1
             bot += 1
         elif capital(word):
-            #Is correct
+            # Is correct
             print("Capital Word --------> " + word)
             top += 1
             bot += 1
@@ -57,7 +67,7 @@ try:
             word = word.replace(",", "")
             word = word.replace("?", "")
             word = word.replace("!", "")
-            if (word in words):
+            if word in words:
                 print("Good Word    --------> " + word)
                 top += 1
             else:
@@ -67,7 +77,7 @@ try:
                         print(word + " --------> " + singletryword)
             bot += 1
         else:
-            #Bad Word, Reccomend
+            # Bad Word, Reccomend
             bot += 1
             tryword = findsimilar(word)
             for singletryword in tryword:
@@ -75,7 +85,7 @@ try:
                     print(word + " --------> " + singletryword)
     print()
     print()
-    percent = top /bot 
+    percent = top / bot
     percent = percent * 100
     percent = int(percent)
     print("#########################")
